3677db510aecbb37209bbbe48a75d23759a1906c
Michal Pawlak <mpawlak.it@gmail.com>
2022-11-04 18:01:22 +0100

    pierwszy commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Menu from './layout/Menu';
+import Home from './layout/Home';
+import Projects from './layout/Projects';
+import Contact from './layout/Contact';
+
+const App = () => {
+	return (
+		<>
+			<Menu />
+			<Home />
+			<Projects />
+			<Contact />
+		</>
+	);
+};
 
 export default App;
