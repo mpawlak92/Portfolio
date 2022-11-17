e3d212424ecc2410b92e18113136a3d667aaaa27
Michal Pawlak <mpawlak.it@gmail.com>
2022-11-17 17:11:01 +0100

    chnge structure of login form and added clssys and styles

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import Modal from '../Modal/Modal';
+import './LoginForm.scss';
 const LoginForm = ({ handleOnClose, isModalActive }) => {
 	return (
 		<Modal
 			handleOnClose={handleOnClose}
 			isOpen={isModalActive}
 			shoulbBeCloseOnOutsideClick={true}>
-			<form>
-				<div>
-					<label>User</label>
-					<input tepe='text' />
-				</div>
-				<div>
-					<label>Password</label>
-					<input tepe='password' />
-				</div>
+			<form className='login-form'>
+				<div className='login-form__error'>Nieprawidłowy login lub hasło.</div>
+				<label className='login-form__label' htmlFor='user'>
+					User:
+				</label>
+				<input className='login-form__input' tepe='text' id='user' />
+				<label className='login-form__label' htmlFor='pass'>
+					Password:
+				</label>
+				<input className='login-form__input' tepe='password' id='pass' />
 			</form>
 		</Modal>
 	);
